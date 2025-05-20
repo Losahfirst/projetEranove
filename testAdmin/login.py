98cc38edacdbc79b4942aaefe226fbe1a7de43cb
KOREKOU EMMANUEL GBRA <92445623+Losahfirst@users.noreply.github.com>
2025-05-20 16:42:50 +0000

    Update login

--- testAdmin/login.py
+++ testAdmin/login.py
@@ -1,5 +1,4 @@
-login = input("Enter your login: ")
-password = input("Enter your password: ")
+
 # This code takes the user's login and password as input.
 # It uses the input function to prompt the user for their credentials.
 
