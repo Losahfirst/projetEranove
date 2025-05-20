3daee9c081e0967b27eb89b83800b76a76acb6c6
KOREKOU EMMANUEL GBRA <92445623+Losahfirst@users.noreply.github.com>
2025-05-20 16:41:27 +0000

    Test1

---
+++ test.py
@@ -0,0 +1,9 @@
+input("Saisir quelque chose : ")
+print("Vous avez saisi quelque chose.")
+
+# This code will wait for the user to input something before printing the message.
+# The input function is used to take input from the user.
+
+calcul = 1 + 1
+print("Le résultat de 1 + 1 est :", calcul)
+# This code performs a simple addition and prints the result.
---
+++ testAdmin/login.py
@@ -0,0 +1,5 @@
+login = input("Enter your login: ")
+password = input("Enter your password: ")
+# This code takes the user's login and password as input.
+# It uses the input function to prompt the user for their credentials.
+
